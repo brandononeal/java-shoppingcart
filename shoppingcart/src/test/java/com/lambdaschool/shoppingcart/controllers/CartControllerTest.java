1c7811f16c2e27301fdefae4c8d4f989c0ec6c4d
brandononeal <baoneal95@gmail.com>
2020-11-13 21:04:33 -0800

    working on testing

---
+++ shoppingcart/src/test/java/com/lambdaschool/shoppingcart/controllers/CartControllerTest.java
@@ -0,0 +1,39 @@
+package com.lambdaschool.shoppingcart.controllers;
+
+import com.lambdaschool.shoppingcart.ShoppingCartApplication;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ShoppingCartApplication.class)
+public class CartControllerTest
+{
+    @Before
+    public void setUp() throws Exception
+    {
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+    }
+
+    @Test
+    public void listCartItemsByUserId()
+    {
+    }
+
+    @Test
+    public void addToCart()
+    {
+    }
+
+    @Test
+    public void removeFromCart()
+    {
+    }
+}
